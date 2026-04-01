4d441ff8bd1c855459f2aa29f4fb1825757f6b10
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-01 13:56:24 -0700

    Start frontend

---
+++ src/lib/components/ControlPanelHeader.svelte
@@ -0,0 +1,13 @@
+<script>
+	const { isReady, status } = $props();
+</script>
+
+<div class="my-2 flex items-center justify-center">
+	<h1 class="text-xl font-bold">BlobSculptor</h1>
+	{#if isReady}
+		<span class="mx-2 h-4 w-4 rounded-full bg-emerald-600"></span>
+	{:else}
+		<span class="mx-2 h-4 w-4 rounded-full bg-rose-600"></span>
+	{/if}
+	<h3 class="text-zinc-300" title="Python Backend connection status">{status}</h3>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { backend, initBackend, getModels } from '$lib/backend.svelte.js';
+	import ControlPanelHeader from '$lib/components/ControlPanelHeader.svelte';
 
 	let modelsList = $state([]);
 	let status = $state('Initializing...');
@@ -9,10 +10,10 @@
 		await initBackend();
 
 		if (backend.isReady) {
-			status = 'Connected to Python Backend';
+			status = 'Connected!';
 			modelsList = await getModels();
 		} else {
-			status = 'Connection failed.';
+			status = 'Failed!';
 		}
 	});
 
@@ -22,17 +23,42 @@
 	}
 </script>
 
-<h2>Backend Status: {status}</h2>
+<div class="flex h-screen w-full overflow-hidden bg-zinc-900 text-zinc-50">
+	<!-- Control Panel -->
+	<div class="flex h-screen w-1/5 flex-col bg-zinc-950">
+		<ControlPanelHeader isReady={backend.isReady} {status} />
+		<hr class="border-zinc-700" />
+	</div>
 
-<button onclick={handlePing} disabled={!backend.isReady}> Ping Python </button>
+	<!-- Model Viewer -->
+	<div class="flex h-screen w-4/5 p-2">
+		{#if modelsList.length > 0}
+			<h3>Available Models:</h3>
+			<ul>
+				{#each modelsList as model}
+					<li>{model}</li>
+				{/each}
+			</ul>
+		{:else if backend.isReady}
+			<p>No .obj models found in assets folder.</p>
+		{/if}
 
-{#if modelsList.length > 0}
-	<h3>Available Models:</h3>
-	<ul>
-		{#each modelsList as model}
-			<li>{model}</li>
-		{/each}
-	</ul>
-{:else if backend.isReady}
-	<p>No .obj models found in assets folder.</p>
-{/if}
+		<!-- Bottom Center Button Panel -->
+		<div class="fixed inset-x-1/2 bottom-0 flex transform justify-center p-4">
+			<div class="flex gap-0.5 rounded-xl border-2 border-zinc-700 bg-zinc-700">
+				<button
+					class="w-32 rounded-l-xl bg-zinc-900 py-2 text-zinc-50 hover:bg-zinc-800"
+					onclick={handlePing}
+				>
+					Hair
+				</button>
+				<button
+					class="w-32 rounded-r-xl bg-zinc-900 py-2 text-zinc-50 hover:bg-zinc-800"
+					onclick={handlePing}
+				>
+					Character
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
