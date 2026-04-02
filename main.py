93e7fe9ca78020d4828f58ccb4990de810f424de
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-01 17:20:30 -0700

    Build dist system

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import webview
 import os
+import sys
 import signal
 from pathlib import Path
 from api import API
 
 ENV = os.getenv("APP_ENV", "production")
-BASE_DIR = Path(__file__).resolve().parent
+
+# Helper to get resources depending on environment
+def get_resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 def run_app():
     api = API()
@@ -15,15 +22,16 @@ def run_app():
         url = "http://localhost:5173"
         print(f"Development mode: Using URL: {url}")
     else:
-        url = str(BASE_DIR / "build" / "index.html")
-        print("Production mode: Using local build")
+        build_dir = get_resource_path("build")
+        url = os.path.join(build_dir, "index.html")
+        print(f"Production mode: Using URL: {url}")
 
     # force ctrl+c
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     # start webview
     window = webview.create_window("BlobSculptor", url, js_api=api)
-    webview.start(http_server=(ENV != "development"), debug=True)
+    webview.start(http_server=(ENV != "development"), debug=(ENV == "development"))
 
 if __name__ == "__main__":
     run_app()
